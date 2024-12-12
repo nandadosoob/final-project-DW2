7271a4690057eb3e19c595eb0933e01f0d8f0f42
nandadosoob <fer.r.benetti@gmail.com>
2024-12-12 15:17:00 -0300

    aaa3

--- server.js
+++ server.js
@@ -72,7 +72,8 @@ server.get("/encomendas", async (request, reply) => {
   // Chama o método list do banco de dados, passando o termo de busca
   const encomendas = await database.list(cliente);
 
-  reply.send(encomendas)
+  reply.json(encomendas)
+  // reply.send(encomendas)
 
 
 });
